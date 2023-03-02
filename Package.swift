8aa320c0ec5947c30f962ca8cd90602a0cb5d238
Edon <edon@valdman.works>
2023-03-02 12:46:32 -0500

    Renamed package from SwiftUIColorConstants
    Renamed package from UIColor+SwiftUI to SwiftUIColorConstants

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "UIColor+SwiftUI",
+    name: "SwiftUIColorConstants",
     platforms: [
         .iOS(.v13),
         .watchOS(.v6),
@@ -15,8 +15,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "UIColor+SwiftUI",
-            targets: ["UIColor+SwiftUI"]),
+            name: "SwiftUIColorConstants",
+            targets: ["SwiftUIColorConstants"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -26,10 +26,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "UIColor+SwiftUI",
+            name: "SwiftUIColorConstants",
             dependencies: []),
         .testTarget(
-            name: "UIColor+SwiftUITests",
-            dependencies: ["UIColor+SwiftUI"]),
+            name: "SwiftUIColorConstantsTests",
+            dependencies: ["SwiftUIColorConstants"]),
     ]
 )
