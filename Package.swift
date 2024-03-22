10c9a2f6a75ad166220f517adc786e66005e1bb4
Edon <edon@valdman.works>
2024-03-22 13:31:04 -0500

    Added empty privacy manifest

--- Package.swift
+++ Package.swift
@@ -27,7 +27,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUIColorConstants",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("../PrivacyInfo.xcprivacy")]
+        ),
         .testTarget(
             name: "SwiftUIColorConstantsTests",
             dependencies: ["SwiftUIColorConstants"]),
